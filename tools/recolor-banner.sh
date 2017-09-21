29952a33cfdf9df3b633ba1ac5d1892663c6c1bf
pseudorook <pseudorook@protonmail.com>
2017-09-21 00:40:37 -0400

    add rump script for recoloring banner

---
+++ tools/recolor-banner.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+set -eu
+
+# # default to blue
+# sed -i "s/#e4e4e4;/#86a7ca;/g" banner.svg
+# sed -i "s/#e3dede;/#7da1c7;/g" banner.svg
+# sed -i "s/#e5d8d8;/#759ac2;/g" banner.svg
+# sed -i "s/#e0d2d2;/#6a92bb;/g" banner.svg
+# sed -i "s/#e0cece;/#658db6;/g" banner.svg
+# sed -i "s/#d8cbcb;/#5784b1;/g" banner.svg
+# sed -i "s/#d6c6c6;/#4a7baa;/g" banner.svg
+# sed -i "s/#d2c0c0;/#3c75aa;/g" banner.svg
+# sed -i "s/#525252;/#11749b;/g" banner.svg
+# sed -i "s/#363636;/#0c536f;/g" banner.svg
+
+# # default to turquoise
+# sed -i "s/#e4e4e4;/#06f1cc;/g" banner.svg
+# sed -i "s/#e3dede;/#0fe1c0;/g" banner.svg
+# sed -i "s/#e5d8d8;/#4cd9be;/g" banner.svg
+# sed -i "s/#e0d2d2;/#36d3be;/g" banner.svg
+# sed -i "s/#e0cece;/#00cfb2;/g" banner.svg
+# sed -i "s/#d8cbcb;/#00c8af;/g" banner.svg
+# sed -i "s/#d6c6c6;/#28c1a8;/g" banner.svg
+# sed -i "s/#d2c0c0;/#00bda0;/g" banner.svg
+# sed -i "s/#525252;/#00a98f;/g" banner.svg
+# sed -i "s/#363636;/#00967f;/g" banner.svg
